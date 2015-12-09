84ffdb866d7e7a0ffe6819c3fa1d9f056408097b
Claire <ClaireS29@gmail.com>
2015-12-09 10:40:08 -0800

    added forms to products show page that is not working. Also added rspecs and a render stars helper method

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,2 +1,17 @@
 class ReviewController < ApplicationController
+
+  def new
+    @review = Review.new
+  end
+
+  def update
+    @review.update(review_params[:review])
+  end
+
+  private
+
+  def review_params
+    params.permit(review: [:product_id, :rating, :comment])
+
+  end
 end
