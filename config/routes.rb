cf1eb31c76f5f9a9728ddbc1fa16a1ff92e761c4
Claire <ClaireS29@gmail.com>
2015-12-09 13:50:52 -0800

    added add to cart form in products show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  post '/products/:id/add' => 'orderitems#add_to_cart'
+
   resources :products do
     resources :reviews
   end
 
-  resources :categories only: [:new, :create]
+  resources :categories, only: [:new, :create]
 
   resources :robots
 
-  resources :orders 
+  resources :orders
 end
