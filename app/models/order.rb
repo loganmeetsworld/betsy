c0aecf7031ecbc75a9a7173acefb13ee512d5c00
Jenna Nichols <jennaplusplus@gmail.com>
2015-12-10 15:47:54 -0800

    method to calculate total number of items

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,11 @@
 class Order < ActiveRecord::Base
   has_many :orderitems
+
+  def total_items
+    total = 0
+    self.orderitems.each do |item|
+      total += item.quantity if !item.quantity.nil?
+    end
+    return total
+  end
 end
--- spec/controllers/orderitems_controller_spec.rb
+++ spec/controllers/orderitems_controller_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe OrderitemsController, type: :controller do
+  describe "POST 'add_to_cart'" do
+    let(:good_params) do
+      {
+        orderitem: {
+          quantity: 5
+        },
+        id: 2
+      }
+    end
+    it "redirects to show view" do
+      post :add_to_cart, good_params
+      expect(subject).to redirect_to product_path(2)
+    end
+  end
+
+  describe "PATCH 'add_to_cart'" do
+    let(:good_params) do
+      {
+        orderitem: {
+          quantity: 5
+        },
+        id: 2
+      }
+    end
+    it "redirects to show view" do
+      post :add_to_cart, good_params
+      expect(subject).to redirect_to product_path(2)
+    end
+  end
+
+  describe "PATCH 'increase_quantity'" do
+    before(:each) do
+      request.env["HTTP_REFERER"] = "where_i_came_from"
+    end
+
+    let(:order) do
+      Order.create(status: "pending")
+    end
+
+    let(:orderitem) do
+      Orderitem.create(order_id: order.id, product_id: 2, quantity: 3)
+    end
+
+    it "refreshes the page" do
+      session[:order_id] = order.id
+      patch :increase_quantity, id: orderitem.id
+      expect(subject).to redirect_to "where_i_came_from"
+    end
+  end
+
+
 
 end
