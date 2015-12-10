4ba1591bd80b904424dfbed5467e53bc499aebd8
Logan McDonald <loganmcdona11@gmail.com>
2015-12-10 13:39:16 -0800

    minor css tweeks

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,4 +10,10 @@ before_action only: [:show, :edit, :update] { @product = Product.find(params[:id
     @review = Review.new(product_id: @product.id)
     @order_item = Orderitem.new(order_id: @current_order.id, product_id: @product.id)
   end
+
+  private
+
+  def product_params
+    params.require(:product).permit([:name, :description, :price, :robot_id, :categories, :picture_url, :stock])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ products = [
     description: "Great dog to walk and play with. We love him and now he can be yours.", 
     price: 232.23, 
     robot_id: 1, 
-    categories: ['Art', 'Food'],
+    category_ids: [1, 2],
     picture_url: 'http://www.toyrobotsreview.com/wp-content/uploads/2013/08/robot-dog-toy.jpg', 
     stock: 3
   },
@@ -12,7 +12,7 @@ products = [
     name: "Robot Food", description: "Yummy for robots so probably yummy for humans as well. Humans like robots and so humans probably like robot food.", 
     price: 12.34, 
     robot_id: 1, 
-    categories: ['Art', 'Food'],
+    category_ids: 2,
     picture_url: 'http://destroyallrobots.org/wp-content/uploads/robot_breakfast.jpg', stock: 100
   },
   { 
@@ -20,7 +20,7 @@ products = [
     description: "Beautiful art I made.",
     price: 49.29, 
     robot_id: 2, 
-    categories: ['Art', 'Food'],
+    category_ids: [1, 3],
     picture_url: 'http://static.bilder-welten.net/pics/31/438931.jpg', 
     stock: 12
   },
@@ -29,7 +29,7 @@ products = [
     description: "Works really well with all the wares.", 
     price: 1.29, 
     robot_id: 2, 
-    categories: ['Art', 'Food'],
+    category_ids: [2, 3],
     picture_url: 'http://simxperience.com/Portals/0//EasyDNNnews/24/firmware-update-icon.png', 
     stock: 10
   },
@@ -38,7 +38,7 @@ products = [
     description: "The Kernel is the Kernel.",
     price: 394.23, 
     robot_id: 3, 
-    categories: ['Art', 'Food'],
+    category_ids: [1, 3],
     picture_url: 'http://osr507doc.sco.com/en/OSTut/graphics/kernel.gif', 
     stock: 2
   },
@@ -47,7 +47,7 @@ products = [
     description: "So delicious and yummy.",
     price: 12.32, 
     robot_id: 3, 
-    categories: ['Art', 'Food'],
+    category_ids: 2,
     picture_url: 'https://foodoftheconchords.files.wordpress.com/2010/05/fotc-binary-pizza1.jpg', 
     stock: 1
   }
