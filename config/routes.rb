5d4598b6344f0e96dc2c66e42654fd78342eb265
Jenna Nichols <jennaplusplus@gmail.com>
2015-12-10 14:21:04 -0800

    update quantity on cart view

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,15 @@ Rails.application.routes.draw do
   patch '/products/:id/add' => 'orderitems#add_to_cart'
   post '/products/:id/add' => 'orderitems#add_to_cart'
 
+  patch 'products/:id/more' => 'orderitems#increase_quantity', as: :more
+  patch 'products/:id/less' => 'orderitems#decrease_quantity', as: :less
+
   resources :products do
     resources :reviews, only: [:new, :create]
   end
 
   resources :categories, only: [:new, :create]
-  
+
   get '/products/robots/:robot_name', to: 'products#robot', as: 'by_robot'
   get '/products/categories/:category_name', to: 'products#category', as: 'by_category'
   resources :robots
