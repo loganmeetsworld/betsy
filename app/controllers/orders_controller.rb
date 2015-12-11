a4d032b4660e604dc7aeb9c9749c57288fb5cb84
Jenna Nichols <jennaplusplus@gmail.com>
2015-12-10 16:15:57 -0800

    destroy order method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,11 @@
 class OrdersController < ApplicationController
   def index
-    
+
+  end
+
+  def destroy
+    @current_order.destroy
+    session[:order_id] = nil
+    redirect_to orders_path
   end
 end
