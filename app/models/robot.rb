29095fc520683491e3e50d36487e623890494224
Logan McDonald <loganmcdona11@gmail.com>
2015-12-11 13:35:42 -0800

    robot tests

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,3 +1,8 @@
 class Robot < ActiveRecord::Base
   has_many :products
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: /@/
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  has_secure_password
 end
--- spec/models/robot_spec.rb
+++ spec/models/robot_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Robot, type: :model do
+  describe "validations" do 
+    before_action do 
+      @good_robot = Robot.create(name: nil, password: nil, )
+    end
+
+    it "requires name" do 
+    end
+
+    it "requires name as case sensitive" do 
+    end
+
+    it "requires unique email" do 
+    end
+
+    it "requires password confirmation to match" do 
+    end
+  end
 end
