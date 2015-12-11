7a5ff6c2c19947cc8d6f813708a657423e00c0db
Logan McDonald <loganmcdona11@gmail.com>
2015-12-10 16:03:06 -0800

    Product model specs working

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  let(:good_product) { build :product, name: nil }
-  let(:bad_product) { build :product, name: "Test" }
+  before :each do
+    @good_product = Product.create(name: 'product', price: 10, robot_id: 1, stock: 1)
+    @bad_product = Product.create(name: nil, price: nil, robot_id: nil, stock: nil)
+  end
 
   describe "validations" do 
     it "has a user id" do 
-      expect(good_product).to be_valid
-      expect(bad_product).to_not be_valid
+      expect(@good_product).to be_valid
+      expect(@bad_product).to_not be_valid
     end
 
     it "has a name" do
-      expect(good_product).to be_valid
-      expect(bad_product).to_not be_valid
-      expect(bad_product.errors.keys).to include(:name)
+      expect(@good_product).to be_valid
+      expect(@bad_product).to_not be_valid
+      expect(@bad_product.errors.keys).to include(:name)
     end
   end
 end
