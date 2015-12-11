2429720505c77631dc12d2ba389742dda1504b3c
Logan McDonald <loganmcdona11@gmail.com>
2015-12-11 14:01:11 -0800

    coverage for order and product

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,4 +3,15 @@ require 'rails_helper'
 RSpec.describe Order, type: :model do
   describe "validations" do 
   end
+
+  describe "total items calculation" do 
+    order = Order.create
+
+    Orderitem.create(product_id: 1, order_id: order.id, quantity: 3)
+    Orderitem.create(product_id: 3, order_id: order.id, quantity: 2)
+
+    it "multiples the item by quantity" do
+      expect(order.total_items).to eq 5
+    end
+  end
 end
