9e1d70b717c1546c9c693cdfb5dab19544c7bfbb
Logan McDonald <loganmcdona11@gmail.com>
2015-12-14 12:13:02 -0800

    schema changed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20151214193824) do
 
   create_table "categories", force: :cascade do |t|
