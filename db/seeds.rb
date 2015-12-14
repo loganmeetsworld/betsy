46f42420bac97b6aeabfe2ce09c9457fc55f6b9f
Claire <ClaireS29@gmail.com>
2015-12-13 20:20:51 -0800

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,21 +38,165 @@ products = [
     stock: 2
   },
   {
-    name: "Binary pizza",
-    description: "So delicious and yummy.",
-    price: 12.32,
-    robot_id: 3,
-    picture_url: 'https://foodoftheconchords.files.wordpress.com/2010/05/fotc-binary-pizza1.jpg',
+    name: "Robot Cat",
+    description: "Will be as apathetic and cuddley as a real cat.",
+    price: 100.50,
+    robot_id: 1,
+    picture_url: 'https://f1.bcbits.com/img/a2219945996_16.jpg',
+    stock: 5
+  },
+  {
+    name: "Robot Koala",
+    description: "How could you not want this? Austrailian inspired design made by (robot) hand",
+    price: 299.99,
+    robot_id: 1,
+    picture_url: 'https://s-media-cache-ak0.pinimg.com/736x/1a/ac/1b/1aac1b41b3fdf0bd43fd7f5e0e457bed.jpg',
+    stock: 4
+  },
+  {
+    name: "Robot Bunny",
+    description: "Soft and perfect for ages 3 - 10",
+    price: 19.99,
+    robot_id: 1,
+    picture_url: 'http://orig12.deviantart.net/bd11/f/2009/186/e/f/sprocket_the_robot_rabbit_by_bibelotforest.jpg',
+    stock: 7
+  },
+  {
+    name: "I, Robot",
+    description: "Titled 'Living with Nature', printed on glossy high def paper 12 ft by 12ft. ",
+    price: 199.99,
+    robot_id: 2,
+    picture_url: 'http://orig04.deviantart.net/0be5/f/2014/263/f/2/robot_art_1_by_aem_88-d7zuxc2.jpg',
     stock: 1
+  },
+  {
+    name: "Robot with a Pearl Earring",
+    description: "Soft and perfect for ages 3 - 10",
+    price: 21.50,
+    robot_id: 2,
+    picture_url: 'http://media02.hongkiat.com/humanoid_robot_artwork/Equilibrium-robot-artwork-by-Daniel-Arnold-Mist.jpg',
+    stock: 3
+  },
+  {
+    name: "Mona Lisa Robot",
+    description: "Impress all your friends with this 8 x 10 print of original art from 10011101011001010",
+    price: 55.25,
+    robot_id: 5,
+    picture_url: 'http://41.media.tumblr.com/431544f1f9f3ed1ae6b2add5d598c97a/tumblr_nlh3nn25Zt1r69vxeo1_500.jpg',
+    stock: 5
+  },
+  {
+    name: "Sketches in Chalk",
+    description: "10 x 12 original charcoal on paper",
+    price: 49.99,
+    robot_id: 2,
+    picture_url: 'http://www.urbanghostsmedia.com/home/twamoran/urbanghostsmedia.com/wp-content/uploads/2015/04/edinburgh-robot-art.jpg',
+    stock: 1
+  },
+  {
+    name: "The Scream",
+    description: "Beautiful mural for home or office use. Delivery requires signature of human.",
+    price: 250.00,
+    robot_id: 2,
+    picture_url: 'http://cdnstatic.visualizeus.com/thumbs/47/19/birds,desert,illustration,robot-47198a8cb759cd9db2981e5684c06e74_h.jpg',
+    stock: 8
+  },
+  {
+    name: "Tablet",
+    description: "Robot designed and child friendly! ",
+    price: 150.00,
+    robot_id: 2,
+    picture_url: 'http://static.ibnlive.in.com/pix/slideshow/03-2013/weekly-roundup-new/main-1-weekly-160313.jpg',
+    stock: 5
+  },
+  {
+    name: "X-ray vision goggles",
+    description: "Easy adjust buckles allow for a custom fit - pull to tighten, lift tab to loosen. 100% UV Protection. Made with an exclusive material softeril that offers long lasting comfort and durability.",
+    price: 104.50,
+    robot_id: 5,
+    picture_url: 'http://images.askmen.com/entertainment/guy_gear/ski-gadgets-2015_1417794395.jpg',
+    stock: 3
+  },
+  {
+    name: "Portable Speaker",
+    description: "Plugs into headphone jack for easy use, also works when not attached to handlebars for a portable speaker. Enjoy your favorite music while cruising with your friends. Speaker will not obsolete when new phone is launched. ",
+    price: 55.25,
+    robot_id: 1,
+    picture_url: 'http://img.timeinc.net/time/2008/top_ten_2008/10_gadgets/pico.jpg',
+    stock: 5
+  },
+  {
+    name: "Botch",
+    description: "Wild watches for wild peopl.",
+    price: 49.99,
+    robot_id: 3,
+    picture_url: 'http://www.webdesignco.co.uk/images/2014/01/top-gadgets-for-designers_eniof_9.jpg',
+    stock: 6
+  },
+  {
+    name: "Nail Drying Station",
+    description: "Home station to dry nail quickly and with no mess. Salon experience in the comfort of your own home.",
+    price: 39.99,
+    robot_id: 1,
+    picture_url: 'http://www.emel.com/images/gadget3_57.jpg',
+    stock: 7
+  },
+  {
+    name: "Arm Phone",
+    description: "Never lose your phone again! Compatible with apple and android. See sales robot for discounts with a 2 year contract.",
+    price: 699.99,
+    robot_id: 5,
+    picture_url: 'https://i.ytimg.com/vi/O3SRoK2UF0I/maxresdefault.jpg',
+    stock: 90
+  },
+  {
+    name: "Robot Cleaner Eggs",
+    description: "Set these loose in your house and they will clean all those hard-to-reach spots. Great for dusting duties and behind furniture. Solar chargable.",
+    price: 80.00,
+    robot_id: 2,
+    picture_url: 'http://cosmouk.cdnds.net/14/32/980x490/nrm_1407142589-beauty-gadgets.jpg',
+    stock: 15
+  },
+  {
+    name: "Calculator Bracelets",
+    description: "Bracelets can perform calculations based on oral commands. Have it in the home or office and never need your fingers to type in calculations again!",
+    price: 104.50,
+    robot_id: 4,
+    picture_url: 'http://static.boredpanda.com/blog/wp-content/uuuploads/photography-gadgets-and-gift-ideas/photography-gadgets-16.jpg',
+    stock: 7
+  },
+  {
+    name: "Pancake Maker",
+    description: "Tell this robot that you want pancakes and voila! Robot can also be set the night before to have pancakes ready in the morning",
+    price: 80.00,
+    robot_id: 2,
+    picture_url: 'http://i.huffpost.com/gen/1328223/thumbs/r-MINIMUM-WAGE-ROBOT-large.jpg',
+    stock: 13
+  },
+  {
+    name: "Robot Taste Maker",
+    description: "Robot learns your personal taste preferences to combine various unique flavors most suited to your own palatte. Indispensible for those 'can't make up your mind' kind of days.",
+    price: 80.00,
+    robot_id: 2,
+    picture_url: 'http://i.ebayimg.com/00/s/NTAwWDUwMA==/z/IUAAAOxy63FSs85W/$_3.JPG?set_id=2',
+    stock: 15
+  },
+  {
+    name: "Robot Oil",
+    description: "Treat your home robots with what they want most! Has proven effects in the longevity and efficacy of your personal home robots.",
+    price: 6.50,
+    robot_id: 4,
+    picture_url: 'http://kimncris.com/bender/robot_oil.jpg',
+    stock: 25
+  },
+  {
+    name: "Robot Gears",
+    description: "Some crunchies for your robot munchies.",
+    price: 8.00,
+    robot_id: 2,
+    picture_url: 'http://www.ilikewallpaper.net/ipad-air-wallpapers/download/20619/Golden-Watch-Gears-ipad-air-wallpaper-ilikewallpaper_com.jpg',
+    stock: 67
   }
-]
-
-
-categories = [
-  { name: "Art" },
-  { name: "Food" },
-  { name: "Software" },
-  { name: "Pets" },
 ]
 
 reviews = [
@@ -85,7 +229,242 @@ reviews = [
     rating: 2,
     product_id: 4,
     comment: "Could not live without this. Literally."
-  }
+  },
+  {
+    rating: 5,
+    product_id: 6,
+    comment: "Cutest cat ever."
+  },
+  {
+    rating: 3,
+    product_id: 6,
+    comment: "Great but I prefer real cats."
+  },
+  {
+    rating: 2,
+    product_id: 7,
+    comment: "Ugh, moves so slow!"
+  },
+  {
+    rating: 4,
+    product_id: 8,
+    comment: "My son loves his robot bunny. AND it doesn't breed. Perfect!"
+  },
+  {
+    rating: 5,
+    product_id: 8,
+    comment: "I love  this product."
+  },
+  {
+    rating: 2,
+    product_id: 9,
+    comment: "Paper is too glossy."
+  },
+  {
+    rating: 1,
+    product_id: 9,
+    comment: "Ugh, not a fan."
+  },
+  {
+    rating: 4,
+    product_id: 10,
+    comment: "So great."
+  },
+  {
+    rating: 8,
+    product_id: 10,
+    comment: "TRALALALALALALALALALALALAlllllas"
+  },
+  {
+    rating: 1,
+    product_id: 3,
+    comment: "Damaged on delivery. Need better human customer service next time."
+  },
+  {
+    rating: 3,
+    product_id: 11,
+    comment: "Sets the mood for my house."
+  },
+  {
+    rating: 3,
+    product_id: 11,
+    comment: "Interesting piece for the entry of our office."
+  },
+  {
+    rating: 1,
+    product_id: 11,
+    comment: "Whatever."
+  },
+  {
+    rating: 1,
+    product_id: 13,
+    comment: "Not what I was expecting!"
+  },
+  {
+    rating: 5,
+    product_id: 13,
+    comment: "Measure first, this thing is BIG!."
+  },
+  {
+    rating: 5,
+    product_id: 14,
+    comment: "I love using this product."
+  },
+  {
+    rating: 4,
+    product_id: 14,
+    comment: "I really love using this product."
+  },
+  {
+    rating: 2,
+    product_id: 15,
+    comment: "Vision isn't as good as expected."
+  },
+  {
+    rating: 3,
+    product_id: 15,
+    comment: "I love using this product but the battery doesn't last very long."
+  },
+  {
+    rating: 3,
+    product_id: 15,
+    comment: "I love using this product."
+  },
+  {
+    rating: 3,
+    product_id: 16,
+    comment: "I love using this product."
+  },
+  {
+    rating: 3,
+    product_id: 16,
+    comment: "It's okay."
+  },
+  {
+    rating: 3,
+    product_id: 16,
+    comment: "I love using this product."
+  },
+  {
+    rating: 4,
+    product_id: 17,
+    comment: "I love using this product."
+  },
+  {
+    rating: 3,
+    product_id: 17,
+    comment: "I like apple watch better."
+  },
+  {
+    rating: 4,
+    product_id: 17,
+    comment: "I love using this product."
+  },
+  {
+    rating: 2,
+    product_id: 18,
+    comment: "Not a fan."
+  },
+  {
+    rating: 4,
+    product_id: 18,
+    comment: "No more ruined nails!"
+  },
+  {
+    rating: 3,
+    product_id: 18,
+    comment: "It's okay but totally unnecessary."
+  },
+  {
+    rating: 3,
+    product_id: 19,
+    comment: "It's okay but totally unnecessary."
+  },
+  {
+    rating: 3,
+    product_id: 19,
+    comment: "I like apple watch better."
+  },
+  {
+    rating: 2,
+    product_id: 19,
+    comment: "Not a fan."
+  },
+  {
+    rating: 4,
+    product_id: 20,
+    comment: "I really love using this product."
+  },
+  {
+    rating: 5,
+    product_id: 20,
+    comment: "So helpful for dusting."
+  },
+  {
+    rating: 4,
+    product_id: 20,
+    comment: "I like it."
+  },
+  {
+    rating: 3,
+    product_id: 21,
+    comment: "I like it."
+  },
+  {
+    rating: 4,
+    product_id: 21,
+    comment: "This product is cool."
+  },
+  {
+    rating: 4,
+    product_id: 22,
+    comment: "This product is cool."
+  },
+  {
+    rating: 5,
+    product_id: 22,
+    comment: "OMG MY NEW FAVORITE THING I USE IT EVERYDAY"
+  },
+  {
+    rating: 4,
+    product_id: 23,
+    comment: "This product is cool."
+  },
+  {
+    rating: 5,
+    product_id: 23,
+    comment: "OMG MY NEW FAVORITE THING I USE IT EVERYDAY"
+  },
+  {
+    rating: 4,
+    product_id: 24,
+    comment: "This product is cool."
+  },
+  {
+    rating: 2,
+    product_id: 24,
+    comment: "You get what you pay for."
+  },
+  {
+    rating: 2,
+    product_id: 25,
+    comment: "You get what you pay for."
+  },
+  {
+    rating: 4,
+    product_id: 25,
+    comment: "This product is cool."
+  },
+]
+
+categories = [
+  { name: "Art" },
+  { name: "Food" },
+  { name: "Gadgets" },
+  { name: "Pets" },
+  { name: "Home" },
+  { name: "Clothing" },
+  { name: "Software" }
 ]
 
 products.each do |product|
@@ -100,9 +479,51 @@ reviews.each do |review|
   Review.create(review)
 end
 
-Product.find(1).categories << Category.find(1)
-Product.find(1).categories << Category.find(2)
-Product.find(1).categories << Category.find(3)
-Product.find(2).categories << Category.find(1)
+Product.find(1).categories << Category.find(4)
+Product.find(1).categories << Category.find(5)
+Product.find(2).categories << Category.find(5)
 Product.find(2).categories << Category.find(2)
-Product.find(3).categories << Category.find(3)
+Product.find(3).categories << Category.find(1)
+Product.find(3).categories << Category.find(5)
+Product.find(4).categories << Category.find(7)
+Product.find(5).categories << Category.find(7)
+Product.find(5).categories << Category.find(3)
+Product.find(6).categories << Category.find(4)
+Product.find(6).categories << Category.find(5)
+Product.find(7).categories << Category.find(4)
+Product.find(7).categories << Category.find(5)
+Product.find(8).categories << Category.find(4)
+Product.find(8).categories << Category.find(5)
+Product.find(9).categories << Category.find(1)
+Product.find(10).categories << Category.find(1)
+Product.find(11).categories << Category.find(1)
+Product.find(12).categories << Category.find(1)
+Product.find(11).categories << Category.find(5)
+Product.find(13).categories << Category.find(1)
+Product.find(13).categories << Category.find(5)
+Product.find(14).categories << Category.find(3)
+Product.find(15).categories << Category.find(3)
+Product.find(15).categories << Category.find(7)
+Product.find(16).categories << Category.find(3)
+Product.find(16).categories << Category.find(5)
+Product.find(17).categories << Category.find(6)
+Product.find(17).categories << Category.find(3)
+Product.find(18).categories << Category.find(5)
+Product.find(15).categories << Category.find(6)
+Product.find(18).categories << Category.find(3)
+Product.find(19).categories << Category.find(5)
+Product.find(19).categories << Category.find(7)
+Product.find(20).categories << Category.find(5)
+Product.find(20).categories << Category.find(3)
+Product.find(21).categories << Category.find(6)
+Product.find(21).categories << Category.find(7)
+Product.find(21).categories << Category.find(3)
+Product.find(22).categories << Category.find(5)
+Product.find(22).categories << Category.find(2)
+Product.find(23).categories << Category.find(2)
+Product.find(23).categories << Category.find(7)
+Product.find(23).categories << Category.find(3)
+Product.find(24).categories << Category.find(2)
+Product.find(24).categories << Category.find(5)
+Product.find(25).categories << Category.find(2)
+Product.find(25).categories << Category.find(5)
