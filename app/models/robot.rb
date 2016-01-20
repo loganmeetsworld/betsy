328fd34cc3a8fa042f544c88231ea0c8d787b974
Jennie Buechner <janina.buechner@gmail.com>
2016-01-20 15:23:10 -0800

    add validations to robot model

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -2,9 +2,11 @@ class Robot < ActiveRecord::Base
   has_many :products
   has_many :orderitems, :through => :products
 
-
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: /@/
   validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :state, presence: true, length: { is: 2 }
+  validates :country, presence: true, length: { is: 2 }
+  validates :postal_code, presence: true, length: { is: 5 }
 
   has_secure_password
 
