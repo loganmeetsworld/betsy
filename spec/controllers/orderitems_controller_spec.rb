4174ee789403ee34aa0e04f53c4e8ef2734c82c1
Jennie Buechner <janina.buechner@gmail.com>
2016-01-21 09:23:36 -0800

    fix orderitems controller spec

--- spec/controllers/orderitems_controller_spec.rb
+++ spec/controllers/orderitems_controller_spec.rb
@@ -82,10 +82,6 @@ RSpec.describe OrderitemsController, type: :controller do
       request.env["HTTP_REFERER"] = "where_i_came_from"
     end
 
-    let(:orderitem) do
-      Orderitem.create(order_id: 1, product_id: 2, quantity: 3, shipped: false)
-    end
-
     it "reverses the shipped status" do
       patch :ship, id: orderitem.id
       expect(orderitem.shipped).to eq false
