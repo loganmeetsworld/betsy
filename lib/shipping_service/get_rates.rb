fbf757dbe4cf44479614f09cb9b0f019f2d2da57
Brittany <BrittanyKohler@gmail.com>
2016-01-21 13:19:08 -0800

    adding get_fedex_rate method to Shipping Services module

--- lib/shipping_service/get_rates.rb
+++ lib/shipping_service/get_rates.rb
@@ -11,19 +11,20 @@ module ShippingService
 
     # return a JSON with correct formatting for the shipping service API
     def get_robot_location(robot)
-      return { "origin" => {"country" => robot.country, "state" => robot.state, "city" => robot.city, "postal_code" => robot.postal_code }}
+      return {"country" => robot.country, "state" => robot.state, "city" => robot.city, "postal_code" => robot.postal_code }
     end
 
     def get_package_location(order)
-      return  { "destination" => {"country" => "US", "state" => order.state, "city" => order.city, "postal_code" => order.zip }}
+      return {"country" => "US", "state" => order.state, "city" => order.city, "postal_code" => order.zip }
     end
 
     def get_package_dimensions(product)
-      return { "package" => {"weight" => product.weight, "length" => product.length, "width" => product.width, "height" => product.height, "cylinder" => product.cylinder }}
+      return {"weight" => product.weight, "length" => product.length, "width" => product.width, "height" => product.height, "cylinder" => product.cylinder }
     end
 
     # HTTParty request
     def get_fedex_rate
+      HTTParty.post("#{BASE_URI}fedex_rates", :headers => { 'Content-Type' => 'application/json' }, :body => { "origin" => @origin, "destination" => @destination,  "package" => @package }.to_json)
     end
 
     # HTTParty request
