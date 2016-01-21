19f1e55501dd0cc4b6af5c7decab6c11e45a45a2
Brittany <BrittanyKohler@gmail.com>
2016-01-21 13:11:28 -0800

    Adding get_robot_location, get_package_location, and get_package_dimensions methods to Shipping Service module

--- lib/shipping_service/get_rates.rb
+++ lib/shipping_service/get_rates.rb
@@ -1,20 +1,25 @@
 module ShippingService
   class Package
 
+    BASE_URI = "https://botsy-shipping.herokuapp.com/"
+
     def initialize(robot, orderitem, product)
-      @robot_loc = get_robot_location(robot)
-      @package_loc = get_package_location(orderitem)
-      @dimensions = get_package_dimensions(product)
+      @origin = get_robot_location(robot)
+      @destination = get_package_location(orderitem)
+      @package = get_package_dimensions(product)
     end
 
     # return a JSON with correct formatting for the shipping service API
     def get_robot_location(robot)
+      return { "origin" => {"country" => robot.country, "state" => robot.state, "city" => robot.city, "postal_code" => robot.postal_code }}
     end
 
-    def get_package_location(orderitem)
+    def get_package_location(order)
+      return  { "destination" => {"country" => "US", "state" => order.state, "city" => order.city, "postal_code" => order.zip }}
     end
 
     def get_package_dimensions(product)
+      return { "package" => {"weight" => product.weight, "length" => product.length, "width" => product.width, "height" => product.height, "cylinder" => product.cylinder }}
     end
 
     # HTTParty request
@@ -24,6 +29,5 @@ module ShippingService
     # HTTParty request
     def get_ups_rate
     end
-
   end
 end
