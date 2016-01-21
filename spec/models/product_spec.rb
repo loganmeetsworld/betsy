89a165eacc15a2617ec47e8f3d5460d34e4c025e
Jennie Buechner <janina.buechner@gmail.com>
2016-01-20 20:33:18 -0800

    add validation tests for product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,41 +3,22 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   before :each do
     @good_product = Product.create(name: 'product', price: 10, robot_id: 1, stock: 1)
-    @bad_product = Product.create(name: nil, price: nil, robot_id: nil, stock: nil)
-    @negative_nums = Product.create(name: 'product', price: -10, robot_id: 1, stock: -1)
   end
 
   describe "validations" do
-    it "has a user id" do
-      expect(@good_product).to be_valid
-      expect(@bad_product).to_not be_valid
-    end
-
-    it "has a name" do
-      expect(@good_product).to be_valid
-      expect(@bad_product).to_not be_valid
-      expect(@bad_product.errors.keys).to include(:name)
-    end
-
-    it "has a robot_id" do
-      expect(@bad_product.errors.keys).to include(:robot_id)
-    end
-
-    it "has a price" do
-      expect(@bad_product.errors.keys).to include(:price)
-    end
-
-    it "price must be greater than 0" do
-      expect(@negative_nums.errors.keys).to include(:price)
-    end
-
-    it "has a stock" do
-      expect(@bad_product.errors.keys).to include(:stock)
-    end
-
-    it "stock must be greater than 0" do
-      expect(@negative_nums.errors.keys).to include(:stock)
-    end
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:robot_id) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_presence_of(:length) }
+    it { is_expected.to validate_presence_of(:width) }
+    it { is_expected.to validate_presence_of(:height) }
+    it { is_expected.to validate_presence_of(:weight) }
+    it { is_expected.to validate_numericality_of(:price).is_greater_than(0)}
+    it { is_expected.to validate_numericality_of(:stock).is_greater_than_or_equal_to(0)}
+    it { is_expected.to validate_numericality_of(:length).is_greater_than(0).is_less_than_or_equal_to(108) }
+    it { is_expected.to validate_numericality_of(:width).is_greater_than(0).is_less_than_or_equal_to(165) }
+    it { is_expected.to validate_numericality_of(:height).is_greater_than(0).is_less_than_or_equal_to(165) }
+    it { is_expected.to validate_numericality_of(:weight).is_greater_than(0).is_less_than_or_equal_to(150) }
   end
 
   describe "model methods" do
