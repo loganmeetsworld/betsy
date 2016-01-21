95344dc529796b3f8c8ff29cf2f44e37fc4ac2de
Jennie Buechner <janina.buechner@gmail.com>
2016-01-20 20:44:07 -0800

    fix product_spec so all tests pass

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,18 @@
 FactoryGirl.define do
 
+  factory :robot do
+
+  end
+
+  factory :product do
+    name 'product'
+    price 10
+    robot_id 1
+    stock 1
+    length 10
+    width 10
+    height 10
+    weight 10
+  end
+
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  before :each do
-    @good_product = Product.create(name: 'product', price: 10, robot_id: 1, stock: 1)
-  end
+  let(:good_product) { create(:product) }
 
   describe "validations" do
     it { is_expected.to validate_presence_of(:name) }
@@ -23,12 +21,12 @@ RSpec.describe Product, type: :model do
 
   describe "model methods" do
     it "averages the rating correctly" do
-      Review.create(rating: 4, product_id: @good_product.id)
-      Review.create(rating: 3, product_id: @good_product.id)
-      Review.create(rating: 3, product_id: @good_product.id)
-      Review.create(rating: 3, product_id: @good_product.id)
+      Review.create(rating: 4, product_id: good_product.id)
+      Review.create(rating: 3, product_id: good_product.id)
+      Review.create(rating: 3, product_id: good_product.id)
+      Review.create(rating: 3, product_id: good_product.id)
 
-      expect(@good_product.rating_average).to eq 3
+      expect(good_product.rating_average).to eq 3
     end
 
     it "shows categories correctly" do
@@ -37,16 +35,16 @@ RSpec.describe Product, type: :model do
       @cat3 = Category.create(name: "cat3")
       @cat4 = Category.create(name: "cat4")
 
-      Product.find(@good_product.id).categories << Category.find(@cat1.id)
-      Product.find(@good_product.id).categories << Category.find(@cat2.id)
-      Product.find(@good_product.id).categories << Category.find(@cat3.id)
-      Product.find(@good_product.id).categories << Category.find(@cat4.id)
+      Product.find(good_product.id).categories << Category.find(@cat1.id)
+      Product.find(good_product.id).categories << Category.find(@cat2.id)
+      Product.find(good_product.id).categories << Category.find(@cat3.id)
+      Product.find(good_product.id).categories << Category.find(@cat4.id)
 
-      expect(@good_product.show_categories).to eq "Cat1, Cat2, Cat3, Cat4"
+      expect(good_product.show_categories).to eq "Cat1, Cat2, Cat3, Cat4"
     end
 
     it "searches products and descriptions for a word" do
-      expect(Product.search("product").name).to eq (@good_product.name.titlecase)
+      expect(Product.search("product").name).to eq (good_product.name.titlecase)
     end
   end
 end
