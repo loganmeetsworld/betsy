09e40940500234c7a10115ba9446ff1db624de67
Jennie Buechner <janina.buechner@gmail.com>
2016-01-20 20:56:03 -0800

    fix robot spec so tests pass

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,18 +1,39 @@
 FactoryGirl.define do
 
-  factory :robot do
+  # @robot = Robot.create(username: 'test', email: 'test@test.com', password: 'test', password_confirmation: 'test')
+  # @product = Product.create(name: 'test', price: 10, robot_id: @robot.id, stock: 1)
+  # @order = Order.create(status: "paid")
+  # @orderitem = Orderitem.create(product_id: @product.id, order_id: @order.id, quantity: 1)
 
+  factory :robot do
+    username 'test'
+    email 'test@test.com'
+    password 'test'
+    password_confirmation 'test'
+    state 'WA'
+    country 'US'
+    postal_code '98133'
   end
 
   factory :product do
     name 'product'
     price 10
-    robot_id 1
     stock 1
     length 10
     width 10
     height 10
     weight 10
+    association :robot
+  end
+
+  factory :order do
+    status 'paid'
+  end
+
+  factory :orderitem do
+    quantity 1
+    association :product
+    association :order
   end
 
 end
--- spec/models/robot_spec.rb
+++ spec/models/robot_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Robot, type: :model do
+  let(:orderitem) { create(:orderitem) }
+  let(:robot) { orderitem.product.robot }
+
   describe "validations" do
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_presence_of(:username) }
@@ -15,12 +18,7 @@ RSpec.describe Robot, type: :model do
 
   describe "revenue method" do
     it "calculates the revenue" do
-      @robot = Robot.create(username: 'test', email: 'test@test.com', password: 'test', password_confirmation: 'test')
-      @product = Product.create(name: 'test', price: 10, robot_id: @robot.id, stock: 1)
-      @order = Order.create(status: "paid")
-      @orderitem = Orderitem.create(product_id: @product.id, order_id: @order.id, quantity: 1)
-
-      expect(@robot.revenue).to eq 10
+      expect(robot.revenue).to eq 10
     end
   end
 end
