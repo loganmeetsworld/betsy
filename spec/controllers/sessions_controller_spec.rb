6c5bf8f5f2a48bf03f01a4096edf4f6857cdd120
Jennie Buechner <janina.buechner@gmail.com>
2016-01-20 22:32:45 -0800

    fix sessions controller spec to pass tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,29 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
- before(:each) do
-    @robot = Robot.create(username: "robot", email: "robot@mail.com", password: "password", password_confirmation: "password")
-  end
+  let(:robot) { create(:robot) }
 
   describe "POST 'create'" do
     let (:session_data) do
       {
-        email: "robot@mail.com",
-        password: "password"
+        email: robot.email,
+        password: robot.password
       }
     end
 
     let (:bad_params) do
-    {
-      session_data:{
-        email: "bad@robot.com", 
-        password: "password"
+      {
+        session_data:{
+          email: "bad@robot.com",
+          password: "password"
+        }
       }
-    }
-  end
+    end
 
     it "creates a session and redirects to robot account page" do
-      robot = Robot.find_by_email(session_data[:email])
       post :create, :session_data => session_data
       expect(response).to redirect_to robot_path(robot)
     end
@@ -34,9 +31,9 @@ RSpec.describe SessionsController, type: :controller do
     end
   end
 
-  describe "DELETE 'destroy" do 
+  describe "DELETE 'destroy" do
     it "allows robot to log out and destroys session" do
-      session[:robot_id] = @robot.id
+      session[:robot_id] = robot.id
       delete :destroy
       expect(session[:robot_id]).to eq(nil)
     end
