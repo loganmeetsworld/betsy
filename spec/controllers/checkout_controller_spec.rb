41bac4dd2f9c4907b15982da4c48288535fc4848
Brittany <BrittanyKohler@gmail.com>
2016-01-22 09:37:45 -0800

    Changing fedex to usps, adding a test for checkout controller

--- spec/controllers/checkout_controller_spec.rb
+++ spec/controllers/checkout_controller_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe CheckoutController, type: :controller do
+  describe "GET 'show'" do
+    let(:params) do
+      {
+        id: "shipping_info",
+      }
+    end
+
+    it "renders the shipping_info page when customer is on shipping_info step" do
+      get :show, params
+      expect(response).to render_template(:shipping_info)
+    end
+  end
+
+  describe "PUT 'update'" do
+    
+  end
+
 end
