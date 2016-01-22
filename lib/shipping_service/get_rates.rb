1835804dcda15a541a0c9c65cfe76b3e07a16cd3
Brittany <BrittanyKohler@gmail.com>
2016-01-22 11:34:14 -0800

    Adding tests for get_rates.rb, removing commented out code from get_rates.rb

--- lib/shipping_service/get_rates.rb
+++ lib/shipping_service/get_rates.rb
@@ -50,48 +50,5 @@ module ShippingService
       end
       return hash
     end
-
-    # We can probably remove all of these, as they were replaced with other methods
-    # to DRY things up a bit.. leaving them here for now, but should delete later
-
-
-    # HTTParty request
-    # def get_fedex_rate(origin, package)
-    #   HTTParty.post("#{BASE_URI}fedex_rates", :headers => { 'Content-Type' => 'application/json' }, :body => { "origin" => origin, "destination" => @destination,  "package" => package }.to_json)
-    # end
-    #
-    # # HTTParty request
-    # def get_ups_rate(origin, package)
-    #   HTTParty.post("#{BASE_URI}ups_rates", :headers => { 'Content-Type' => 'application/json' }, :body => { "origin" => origin, "destination" => @destination,  "package" => package }.to_json)
-    # end
-
-
-    # def get_ups_total
-    #   ups_total_hash = Hash.new(0)
-    #   @orderitems.each do |item|
-    #     origin = get_robot_location(item.product.robot)
-    #     package = get_package_dimensions(item.product)
-    #     ups_array = get_ups_rate(origin, package)
-    #     ups_hash = make_hash(ups_array)
-    #     ups_hash.each do |key, val|
-    #       ups_total_hash[key] += val
-    #     end
-    #   end
-    #   return ups_total_hash
-    # end
-
-    # def get_fedex_total
-    #   fedex_total_hash = Hash.new(0)
-    #   @orderitems.each do |item|
-    #     origin = get_robot_location(item.product.robot)
-    #     package = get_package_dimensions(item.product)
-    #     fedex_array = get_fedex_rate(origin, package)
-    #     fedex_hash = make_hash(fedex_array)
-    #     fedex_hash.each do |key, val|
-    #       fedex_total_hash[key] += val
-    #     end
-    #   end
-    #   return fedex_total_hash
-    # end
   end
 end
---
+++ spec/shipping_service/get_rates_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe ShippingService::Order do
+  let(:orderitem) { create(:orderitem) }
+  let(:product) { orderitem.product }
+  let(:order) { orderitem.order }
+  let(:robot) { product.robot }
+  let(:my_order) {ShippingService::Order.new(order)}
+
+  describe "initialize(order)" do
+    it "creates a new instance of ShippingService::Order" do
+      expect(ShippingService::Order.new(order)).to be_an_instance_of ShippingService::Order
+    end
+  end
+
+  describe "get_robot_location(robot)" do
+    it "returns correctly formatted JSON" do
+      json_response = {"country"=>"US", "state"=>"WA", "city"=>"Seattle", "postal_code"=>"98133"}
+      expect(my_order.get_robot_location(robot)).to eq json_response
+    end
+  end
+
+  describe "get_package_dimensions(product)" do
+    it "returns correctly formatted JSON" do
+      json_response = {"weight"=>10, "length"=>10, "width"=>10, "height"=>10, "cylinder"=>false}
+      expect(my_order.get_package_dimensions(product)).to eq json_response
+    end
+  end
+
+  describe "get_rates(origin, package, carrier)" do
+    it "returns an array of shipping options and rates" do
+      origin = my_order.get_robot_location(robot)
+      package = my_order.get_package_dimensions(product)
+      expect(my_order.get_rates(origin, package, "ups")).to be_an_instance_of Array
+    end
+  end
+
+  describe "get_total(carrier)" do
+    it "returns a hash" do
+      expect(my_order.get_total("ups")).to be_an_instance_of Hash
+    end
+  end
+end
